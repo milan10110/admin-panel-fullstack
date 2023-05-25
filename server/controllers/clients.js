7db51fbbb91473c590b7f609514a99483727c5ad
milan10110 <meelaanvaghasiya00@gmail.com>
2023-05-25 15:16:05 +0530

    Implement /geography route

--- server/controllers/clients.js
+++ server/controllers/clients.js
@@ -1,3 +1,4 @@
+import getCountryIso3 from "country-iso-2-to-3";
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import Transaction from "../models/Transaction.js";
@@ -76,4 +77,29 @@ async function getTransactions(req, res) {
   }
 }
 
-export { getProducts, getCustomers, getTransactions };
+async function getGeography(req, res) {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export { getProducts, getCustomers, getTransactions, getGeography };
